56fea61cd2fc074ff28c91cbf94f7b8eddba2e20
KritiMalani <85431397+KritiMalani@users.noreply.github.com>
2021-06-26 14:58:35 +0530

    Create Methods&Packages.java

---
+++ Methods&Packages.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class project2
+{
+    void convert()
+    {
+    Scanner sc= new Scanner(System.in);
+    int min, years, days;
+    years= 3456789/ 525600;
+    days= 3456789/ 1440;
+    System.out.println("number of years is"+years);
+    System.out.println("number of days is"+days);
+}
+} //close of class
